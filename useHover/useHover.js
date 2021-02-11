1871b53af917c1782c3b245a529b54dc0aeaa3ef
Ki <beomki69@gmail.com>
2021-02-11 17:30:08 +0900

    useHover

---
+++ useHover/useHover.js
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from "react";
+
+export const useHover = (onClick) => {
+  if (typeof onClick !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("mouseenter", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("mouseenter", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
